7039e7c96c1b758f6b1ea424828e217f04c9d110
MigueLEsPaul <miguelhinojosa994@gmail.com>
2020-05-31 18:32:37 -0400

    ADD: Nuevos spiders genéricos para cada uno de los dos tipos de tienda

--- start.py
+++ start.py
@@ -5,16 +5,19 @@ from foodwatch.spiders.caminos_spider import CaminosSpider
 from foodwatch.spiders.carlos3_spider import Carlos3Spider
 # from foodwatch.spiders.quinta_spider import QuintaSpider
 from foodwatch.spiders.quintaalt_spider import QuintaaltSpider
-
-
+from foodwatch.spiders.tuenvio_spider import TuEnvioSpider
+from foodwatch.spiders.xetid_spider import XetidSpider
 settings = get_project_settings()
 process = CrawlerProcess(settings)
 # process = CrawlerProcess()
 
-process.crawl(Carlos3Spider)
-process.crawl(CaminosSpider)
+# process.crawl(Carlos3Spider)
+# process.crawl(CaminosSpider)
 # process.crawl(QuintaSpider)
-process.crawl(QuintaaltSpider)
+# process.crawl(QuintaaltSpider)
+
+process.crawl(TuEnvioSpider)
+process.crawl(XetidSpider)
 
 print('foodwatch [CRAWLING ...]')
 process.start()     # the script will block here until all crawling jobs are finished
