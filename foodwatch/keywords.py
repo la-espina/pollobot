871f8b3258b28a7ba7f40b3ad282422e1729f2d8
MigueLEsPaul <miguelhinojosa994@gmail.com>
2020-05-31 16:36:19 -0400

    MOD: añadidas las urls de cada departamento por tienda

--- foodwatch/keywords.py
+++ foodwatch/keywords.py
@@ -13,8 +13,31 @@ wordpool = [
     "hígado",
     "mollejas",
     "carne",
-    # "salchichas",
+    "salchichas",
+    "salchicha",
     "golden maple",
     "yogourt",
-    "yogurt"
+    "yogurt",
+    'cerveza',
+    'refresco',
+    'refresocs',
+    'jabón',
+    'jabon',
+    'jabones',
+    'agua',
+    'galleta',
+    'pescado',
+    'shampoo'
 ]
+
+# departments by stores 
+departments={
+    '4caminos':[54,46081,60002,52,57,633009,62002,46087,55,56,62006,77014,62001,46077,46080,100002,60013,46079,46078],
+    'carlos3':[54,46081,60002,52,57,633009,62002,46087,55,56,62006,77014,62001,46077,46080,100002,60013,46079,46078,60005,60015,60011,77015 ],
+    'tipicaboyeros':[54,46081,60002,52,57,633009,62002,46087,55,56,62006,77014,62001,46077,46080,100002,60013,46079,46078,46095],
+    'caribehabana':[46087]
+}
+
+xetidep={
+    '5tay42':["100001-alimentos","100012-bebidas-y-licores"]
+}
